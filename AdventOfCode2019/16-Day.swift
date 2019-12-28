b8d4f28fb618a8fb28549de443825a4e15ecf2c5
Felizia Bernutz <felizia.bernutz@gmx.de>
2019-12-28 23:39:49 +0100

    Add Day 16 - Part 2

--- AdventOfCode2019/16-Day.swift
+++ AdventOfCode2019/16-Day.swift
@@ -8,9 +8,9 @@ enum Day16 {
     static func solve() {
         let input = Input.get("16-Input.txt")
         print(Date())
-        print("Result Day 16 - Part One: \(fftAfter100PhasesForPart1(input: input))")
+//        print("Result Day 16 - Part One: \(fftAfter100PhasesForPart1(input: input))")
+        print("Result Day 16 - Part Two: \(fftAfter100PhasesWithMessageOffsetForPart2(input: input))")
         print(Date())
-        //        print("Result Day 16 - Part Two: \(findExactPositionInTimeForPart2(input: input))")
     }
 
     private static func fftAfter100PhasesForPart1(input: String) -> String {
@@ -28,6 +28,28 @@ enum Day16 {
         return result
     }
 
+    private static func fftAfter100PhasesWithMessageOffsetForPart2(input: String) -> String {
+        let inputs = input.compactMap { Int(String($0)) }
+
+        let lengthOfOffset = 7
+        let offset = Int(
+            inputs
+                .prefix(lengthOfOffset)
+                .map { String($0) }
+                .joined()
+            )!
+        var currentOutput = Array([[Int]](repeating: inputs, count: 10000).flatMap { $0 }[offset...])
+
+        let numberOfPhases = 100
+        for _ in 0..<numberOfPhases {
+            currentOutput.makeTransformation2()
+        }
+
+        let result = currentOutput.prefix(8).map { String($0) }.joined()
+        assert(result == "14288025")
+        return result
+    }
+
 }
 
 private extension Array where Element == Int {
@@ -38,16 +60,25 @@ private extension Array where Element == Int {
         for lineIndex in 0..<count {
             var transformed = 0
 
-            for (index, element) in enumerated() {
-                // 1. multiply value in array with pattern
-                // 2. adding up results
-                transformed += element * currentPattern(for: lineIndex, index: index)!
+            for index in 0..<count {
+                transformed += self[index] * currentPattern(for: lineIndex, index: index)!
             }
 
             transformedNumbers.append(transformed.onesDigit)
         }
 
         self = transformedNumbers
+    }
+
+    // this helped:
+    // https://imgur.com/wAJ1zEj
+    // https://www.reddit.com/r/adventofcode/comments/ebai4g/2019_day_16_solutions/
+    mutating func makeTransformation2() {
+        var sum = 0
+        for index in (0..<count).reversed() {
+            sum += self[index]
+            self[index] = sum.onesDigit
+        }
     }
 
     func currentPattern(for lineIndex: Int, index: Int) -> Int? {
@@ -73,7 +104,7 @@ private extension Array where Element == Int {
 
 private extension Int {
     var onesDigit: Int {
-        return String(describing: self).compactMap { Int(String($0)) }.last!
+        return self % 10
     }
 }
 
