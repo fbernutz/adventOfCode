948cf20b14f4c4527bafd890de6dfdd7c6e45afb
Felizia Bernutz <felizia.bernutz@gmx.de>
2020-12-02 09:03:10 +0100

    Refactor Day 02

--- AdventOfCode2020/02-Day.swift
+++ AdventOfCode2020/02-Day.swift
@@ -12,43 +12,38 @@ enum Day02 {
 	}
 
 	struct PasswordPolicy {
-		let minimumCount: Int
-		let maximumCount: Int
+		let firstInformation: Int
+		let secondInformation: Int
 		let character: String
+		let password: String
 
-		/// e.g. string equals `1-3 a`
+		/// e.g. string equals `1-3 a: abcde`
 		init(from input: String) {
-			let parts = input.split(separator: "-")
-			minimumCount = Int(parts[0])!
+			password = input
+				.split(separator: ":")
+				.map(String.init)
+				.last!
+				.trimmingCharacters(in: .whitespaces)
+			let parts = input
+				.split(separator: ":")
+				.first!
+				.split(separator: "-")
+				.map(String.init)
+			firstInformation = Int(parts[0])!
 			let otherParts = parts[1].split(separator: " ")
-			maximumCount = Int(otherParts[0])!
+			secondInformation = Int(otherParts[0])!
 			character = String(otherParts[1])
 		}
 
-		func isValidPassword(_ password: String) -> Bool {
+		func isValidPassword() -> Bool {
 			let characterCount = password.filter { String($0) == character }.count
-			let range = minimumCount...maximumCount
+			let range = firstInformation...secondInformation
 			return range.contains(characterCount)
 		}
-	}
 
-	struct PasswordPolicyNew {
-		let firstPosition: Int
-		let secondPosition: Int
-		let character: String
-
-		/// e.g. string equals `1-3 a`
-		init(from input: String) {
-			let parts = input.split(separator: "-")
-			firstPosition = Int(parts[0].trimmingCharacters(in: .whitespaces))!
-			let otherParts = parts[1].split(separator: " ")
-			secondPosition = Int(otherParts[0].trimmingCharacters(in: .whitespaces))!
-			character = String(otherParts[1].trimmingCharacters(in: .whitespaces))
-		}
-
-		func isValidPassword(_ password: String) -> Bool {
-			let firstPositionCharacter = password[firstPosition - 1]
-			let secondPositionCharacter = password[secondPosition - 1]
+		func isValidPasswordForNewPolicy() -> Bool {
+			let firstPositionCharacter = password[firstInformation - 1]
+			let secondPositionCharacter = password[secondInformation - 1]
 			return (firstPositionCharacter == character
 				|| secondPositionCharacter == character)
 				&& firstPositionCharacter != secondPositionCharacter
@@ -57,38 +52,30 @@ enum Day02 {
 
 	private static func getNumberOfValidPasswords(input: String) -> String {
 		// 1-3 a: abcde
-		var validPasswordCount = 0
-		let lines = input.components(separatedBy: .newlines)
+		let passwordPolicies = input.components(separatedBy: .newlines)
 			.dropLast()
-		for line in lines {
-			let splittedLine = line
-				.split(separator: ":")
-				.map(String.init)
-			let passwordPolicy = PasswordPolicy(from: splittedLine[0])
-			let password = splittedLine[1]
-			if passwordPolicy.isValidPassword(password) {
-				validPasswordCount += 1
-			}
-		}
+			.map(PasswordPolicy.init)
+
+		let validPasswordCount = passwordPolicies
+			.map { $0.isValidPassword() }
+			.filter { $0 == true }
+			.count
+
 		// 569
 		return String(validPasswordCount)
 	}
 
 	private static func getNumberOfValidPasswordsForNewPolicy(input: String) -> String {
 		// 1-3 a: abcde
-		var validPasswordCount = 0
-		let lines = input.components(separatedBy: .newlines)
+		let passwordPolicies = input.components(separatedBy: .newlines)
 			.dropLast()
-		for line in lines {
-			let splittedLine = line
-				.split(separator: ":")
-				.map(String.init)
-			let passwordPolicy = PasswordPolicyNew(from: splittedLine[0])
-			let password = splittedLine[1].trimmingCharacters(in: .whitespaces)
-			if passwordPolicy.isValidPassword(password) {
-				validPasswordCount += 1
-			}
-		}
+			.map(PasswordPolicy.init)
+
+		let validPasswordCount = passwordPolicies
+			.map { $0.isValidPasswordForNewPolicy() }
+			.filter { $0 == true }
+			.count
+
 		// 346
 		return String(validPasswordCount)
 	}
