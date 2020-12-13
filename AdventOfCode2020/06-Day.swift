13db6ade1af1a84948827d38b88311b3e62cfa29
Felizia Bernutz <felizia.bernutz@gmx.de>
2020-12-13 23:23:10 +0100

    Add Day 06 Part 2

--- AdventOfCode2020/06-Day.swift
+++ AdventOfCode2020/06-Day.swift
@@ -8,7 +8,7 @@ enum Day06 {
 	static func solve() {
 		let input = Input.get("06-Input.txt")
 		print("Result Day 6 - Part One: \(numberOfQuestionsAnsweredWithYes(for: input))")
-//		print("Result Day 6 - Part Two: \(findSeat(for: input))")
+		print("Result Day 6 - Part Two: \(numberOfQuestionsAnsweredWithYesByEveryone(for: input))")
 	}
 
 	private static func numberOfQuestionsAnsweredWithYes(for input: String) -> String {
@@ -25,4 +25,26 @@ enum Day06 {
 		return String(count)
 	}
 
+	private static func numberOfQuestionsAnsweredWithYesByEveryone(for input: String) -> String {
+		let answerGroups = input
+			.components(separatedBy: "\n\n")
+			.filter { !$0.isEmpty }
+			.map {
+				$0
+					.split(separator: "\n")
+					.map(Set.init)
+			}
+
+		var count = 0
+		for group in answerGroups {
+			// check if answer is answered by everybody in the group
+			let commonElements = group.reduce(group.first ?? []) {
+				$0.intersection($1)
+			}
+			count += commonElements.count
+		}
+
+		// 3356
+		return String(count)
+	}
 }
