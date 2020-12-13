8362045e2e65e2096377a402071682cf9d829ffc
Felizia Bernutz <felizia.bernutz@gmx.de>
2020-12-13 13:10:08 +0100

    Add Day 13 Part 02
    no result, runs veeeeery long

--- AdventOfCode2020/13-Day.swift
+++ AdventOfCode2020/13-Day.swift
@@ -7,8 +7,11 @@ https://adventofcode.com/2020/day/13
 enum Day13 {
 	static func solve() {
 		let input = Input.get("13-Input.txt")
+		let start = Date()
+		print(start)
 		print("Result Day 13 - Part One: \(findNextBus(for: input))")
 		print("Result Day 13 - Part Two: \(findEarliestTimestampForConstraint(for: input))")
+		print("duration: \(DateInterval(start: start, end: Date()).duration) sec")
 	}
 
 	private static func findNextBus(for input: String) -> String {
@@ -38,6 +41,41 @@ enum Day13 {
 	}
 
 	private static func findEarliestTimestampForConstraint(for input: String) -> String {
+		let busIds = getBusIds(for: input)
+
+		// set timestamp to first bus id after 0
+		let firstBusId = Int(busIds[0])!
+		var currentTimestamp = firstBusId
+
+		// check other busses
+		while !otherBussesAreValid(busIds, currentTimestamp: currentTimestamp) {
+			currentTimestamp += firstBusId
+		}
+
+		return String("\(currentTimestamp)")
+	}
+
+	private static func otherBussesAreValid(_ busIds: [String], currentTimestamp: Int) -> Bool {
+		var currentTime = currentTimestamp
+
+		for busId in busIds {
+			if busId == "x" {
+				// always valid
+				currentTime += 1
+				continue
+			} else if currentTime % Int(busId)! != 0 {
+				// invalid, test failed
+				return false
+			} else {
+				// valid bus, check next
+				currentTime += 1
+			}
+		}
+
+		return true
+	}
+
+	private static func getBusIds(for input: String) -> [String] {
 		let lines = input.components(separatedBy: .newlines)
 			.filter { !$0.isEmpty }
 
@@ -45,10 +83,7 @@ enum Day13 {
 			.split(separator: ",")
 			.map(String.init)
 
-		var minimumTimestamp = 0
-
-		// TODO:
-
-		return String("\(minimumTimestamp)")
+		// 17,x,13,19
+		return busIds
 	}
 }
